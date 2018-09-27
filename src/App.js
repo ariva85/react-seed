ba356611f7a3a526eda45dfe21ac78c73dae4626
a.riva <Alessandro.Riva@scai-consulting.grupposcai.it>
2018-09-27 16:33:03 +0200

    ejected + config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from 'logo.svg';
+import 'App.css';
+import Test from 'components/test';
 
 class App extends Component {
 	render() {
---
+++ src/components/test.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+const test = () => <p>test</p>;
+
+export default test;
